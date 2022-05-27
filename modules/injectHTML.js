1fae513d0d5d628d129f7d30876cfe9f009edc20
dylanoconnor2 <104481875+dylanoconnor2@users.noreply.github.com>
2022-05-27 10:38:37 +1000

    Added Name

--- modules/injectHTML.js
+++ modules/injectHTML.js
@@ -1,12 +1,12 @@
 /**
- * Function injects specified HTML file to specified HTML 
+ * Function injects specified HTML file to specified HTML
  * node of the current file
- * 
+ *
  * @param filePath - a path to a source HTML file to inject
- * @param elem - an HTML element to which this content will 
+ * @param elem - an HTML element to which this content will
  * be injected
  */
- async function injectHTML(filePath,elem) {
+async function injectHTML(filePath, elem, name) {
     try {
         const response = await fetch(filePath);
         if (!response.ok) {
@@ -14,24 +14,23 @@
         }
         const text = await response.text();
         elem.innerHTML = text;
+        if (name) {
+            document.querySelector("h1").textContent = name;
+        }
         // reinject all <script> tags
         // for each <script> tag on injected html
-        elem.querySelectorAll("script").forEach(script => {
+        elem.querySelectorAll("script").forEach((script) => {
             // create a new empty <script> tag
             const newScript = document.createElement("script");
-            // copy attributes of existing script tag 
+            // copy attributes of existing script tag
             // to a new one
-            Array.from(script.attributes).forEach(attr =>
-                newScript.setAttribute(attr.name, attr.value)
-            );
-            // inject a content of existing script tag 
+            Array.from(script.attributes).forEach((attr) => newScript.setAttribute(attr.name, attr.value));
+            // inject a content of existing script tag
             // to a new one
-            newScript.appendChild(
-                document.createTextNode(script.innerHTML)
-            )
+            newScript.appendChild(document.createTextNode(script.innerHTML));
             // replace existing script tag to a new one
             script.parentNode.replaceChild(newScript, script);
-        })
+        });
     } catch (err) {
         console.error(err.message);
     }
@@ -40,15 +39,14 @@
 /**
  * Function used to process all HTML tags of the following
  * format: <div include="<filename>"></div>
- * 
+ *
  * This function injects a content of <filename> to
  * each div with the "include" attribute
  */
 function injectAll() {
-    document.querySelectorAll("[include]")
-            .forEach((elem) => {
-                injectHTML(elem.getAttribute("include"),elem);
-    })
+    document.querySelectorAll("[include]").forEach((elem) => {
+        injectHTML(elem.getAttribute("include"), elem, elem.getAttribute("name"));
+    });
 }
 
 injectAll();
